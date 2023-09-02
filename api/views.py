313ff43ebfc5f2aa5f80c7666639719a8724b68e
Cory DeGuzman <deguzmancory@ymail.com>
2023-09-02 15:15:38 -0700

    defined initial route views

--- api/views.py
+++ api/views.py
@@ -4,4 +4,36 @@ from django.http import JsonResponse
 
 # Create your views here.
 def getRoutes(request):
-    return JsonResponse("HEMLO FROG.", safe=False)
+    routes = [
+        {
+            "endpoint": "/notes/",
+            "method": "GET",
+            "body": None,
+            "description": "Returns an array of notes",
+        },
+        {
+            "endpoint": "/notes/id",
+            "method": "GET",
+            "body": "None",
+            "description": "Returns a single note object",
+        },
+        {
+            "endpoint": "/notes/create/",
+            "method": "POST",
+            "body": {"body": ""},
+            "description": "Creates a new note with data sent in post request",
+        },
+        {
+            "endpoint": "/notes/id/update/",
+            "method": "PUT",
+            "body": {"body": ""},
+            "description": "Creates an existing note with data sent in post request",
+        },
+        {
+            "endpoint": "/notes/id/delete/",
+            "method": "DELETE",
+            "body": None,
+            "description": "Deletes an existing note",
+        },
+    ]
+    return JsonResponse(routes, safe=False)
